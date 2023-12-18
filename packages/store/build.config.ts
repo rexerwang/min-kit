423ed17295162749cb4f89917a29fd3862131309
rexerwang <rexercn@gmail.com>
2023-12-18 22:27:43 +0800

    chore(store): build.config

--- packages/store/build.config.ts
+++ packages/store/build.config.ts
@@ -1,10 +1,7 @@
 import { readFile } from 'node:fs/promises'
-import { dirname, resolve } from 'node:path'
-import { fileURLToPath } from 'node:url'
+import { resolve } from 'node:path'
 
 import { defineBuildConfig } from 'unbuild'
-
-const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default defineBuildConfig({
   hooks: {
