5d1bbf5f9e94eb5e3af6960146b426ae50807488
rexerwang <rexercn@gmail.com>
2023-12-19 03:07:03 +0800

    feat(template): examples

--- miniapp/template/src/pages/index/index.tsx
+++ miniapp/template/src/pages/index/index.tsx
@@ -1,16 +1,38 @@
 import './index.scss'
 
+import { ShareButton } from '@miniapp/components'
+import { logger, toast } from '@miniapp/extends'
+import { Route } from '@miniapp/shared'
 import { Text, View } from '@tarojs/components'
 import { useLoad } from '@tarojs/taro'
 
+import http from '@/http'
+
 export default function Index() {
-  useLoad(() => {
-    console.warn('Page loaded.')
+  useLoad(async () => {
+    logger.debug('Route.generate', Route.generate('pages/index/index', { a: 1, b: 2 }))
+
+    const res = await http.get('/get')
+    await toast.success()
+    logger.debug('#httpbin', res.data)
+
+    try {
+      await http.get('/status/500')
+    } catch (e) {
+      toast.error()
+      logger.error('#httpbin', e)
+    }
   })
 
   return (
     <View className='index'>
       <Text>Hello world!</Text>
+
+      <ShareButton message={{ title: 'share from ShareButton' }} />
     </View>
   )
 }
+
+definePageConfig({
+  enableShareAppMessage: true,
+})
