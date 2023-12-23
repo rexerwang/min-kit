41ab1a8ea67c7495d03f9d3a545779b6e4f7cb76
rexerwang <rexercn@gmail.com>
2023-12-23 12:35:21 +0800

    fix(extends): fix RewardedVideoAd.show & scene enum

--- packages/extends/src/RewardedVideoAd.ts
+++ packages/extends/src/RewardedVideoAd.ts
@@ -11,41 +11,37 @@ class RewardedVideoAd {
   done = false
 
   constructor(private adUnitId: string) {
-    this.eventName = 'RewardedVideoAd#' + adUnitId
+    this.eventName = 'RewardedVideoAdOnClose#' + adUnitId
 
-    this.ad = createRewardedVideoAdApi({ adUnitId: this.adUnitId })
+    this.ad = createRewardedVideoAdApi({ adUnitId })
     this.ad.onError((e) => {
       this.error = e
       logger.error('#ad', '#RewardedVideoAd', 'onError', this.adUnitId, e)
     })
     this.ad.onLoad(() => {
+      this.error = null
       logger.debug('#ad', '#RewardedVideoAd', 'onLoad', this.adUnitId)
     })
     this.ad.onClose((e) => {
-      this.done = !!e.isEnded
-      eventCenter.trigger(this.eventName, !!e.isEnded)
+      this.done = !!e?.isEnded
+      eventCenter.trigger(this.eventName, this.done)
       logger.debug('#ad', '#RewardedVideoAd', 'onClose', this.adUnitId, e)
     })
+  }
+
+  private showAd() {
+    return this.ad.show().catch(() => this.ad.load().then(() => this.ad.show()))
   }
 
   show() {
     return new Promise<boolean>((resolve) => {
       eventCenter.off(this.eventName).once(this.eventName, resolve)
 
-      toast.loading()
-      this.ad
-        .show()
-        // retry
-        .catch(() => this.ad.load().then(() => this.ad.show()))
-        // skip error
-        .catch((e) => {
-          resolve(true)
-          this.error = e
-          logger.error('#ad', '#RewardedVideoAd', 'show', this.adUnitId, e)
-        })
-        .then(() => {
-          toast.clear()
-        })
+      toast
+        .loading()
+        .then(() => this.showAd())
+        .catch(() => resolve(true))
+        .then(toast.clear)
     })
   }
 
--- packages/extends/src/scene.ts
+++ packages/extends/src/scene.ts
@@ -2,7 +2,7 @@ import { attempt } from '@min-kit/shared'
 import { getEnterOptionsSync } from '@tarojs/taro'
 
 /** @see https://developers.weixin.qq.com/miniprogram/dev/reference/scene-list.html */
-export const enum Scene {
+const enum Scene {
   SINGLE_CHAT = 1007,
   GROUP_CHAT = 1008,
   QRCODE_SCAN = 1011,
@@ -20,5 +20,14 @@ export const scene = {
   },
   get isSinglePageMode() {
     return scene.current === Scene.TIMELINE_TO_SINGLE_PAGE
+  },
+  get isFromSubscription() {
+    return [Scene.SUBSCRIPTION, Scene.SUBSCRIPTION2].includes(scene.current)
+  },
+  get isFromShare() {
+    return [Scene.SINGLE_CHAT, Scene.GROUP_CHAT, Scene.FROM_SINGLE_PAGE_MODE].includes(scene.current)
+  },
+  get isFromQrcode() {
+    return [Scene.QRCODE_LONG_PRESS, Scene.QRCODE_SCAN, Scene.QRCODE_SCAN_FROM_ALBUM].includes(scene.current)
   },
 }
