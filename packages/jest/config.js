ad477bc2c13c23aa54def9e70ceb322f7eaf9811
rexerwang <rexercn@gmail.com>
2023-12-31 18:10:24 +0800

    test: use happy-dom instead of jsdom

--- packages/jest/config.js
+++ packages/jest/config.js
@@ -1,10 +1,10 @@
 const defineJestConfig = require('@tarojs/test-utils-react/dist/jest.js').default
 
 module.exports = defineJestConfig({
-  setupFilesAfterEnv: [require.resolve('./config/setupAfterEnv.js')],
-  testEnvironment: 'jsdom',
-  testMatch: ['<rootDir>/src/__tests__/**/*.test.ts?(x)'],
   globals: require('./config/globals'),
+  setupFilesAfterEnv: [require.resolve('./config/setupAfterEnv.js')],
+  testEnvironment: '@happy-dom/jest-environment', // 'jsdom'
+  testMatch: ['<rootDir>/src/__tests__/**/*.test.ts?(x)'],
   collectCoverageFrom: ['<rootDir>/src/**/*.ts?(x)', '!**/__tests__/**', '!**/{debounce,throttle}.ts'],
   coverageReporters: ['clover', 'json', 'lcov', 'text-summary'],
   passWithNoTests: true,
