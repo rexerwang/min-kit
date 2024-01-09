86ae9c6908c939ed5f01bee31584784800d4d257
rexerwang <rexercn@gmail.com>
2024-01-09 14:46:22 +0800

    chore(example): config/mode

--- packages/example/config/mode/dev.ts
+++ packages/example/config/mode/dev.ts
@@ -3,8 +3,8 @@ import type { UserConfig } from '@min-kit/helper/compile'
 export default {
   appid: {
     weapp: process.env.TARO_APP_ID || 'your-weapp-appid', // from dotenv or set the weapp appid
-    tt: process.env.TARO_APP_ID || 'your-tt-appid',
-    alipay: process.env.TARO_APP_ID || 'your-alipay-appid',
+    tt: 'testAppId',
+    alipay: 'testAppId',
   },
   defineConstants: {
     baseUrl: 'https://httpbin.org',
