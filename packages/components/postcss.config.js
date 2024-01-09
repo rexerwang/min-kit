ade63c19e4425709ff4686dc2cf23378b5ececef
rexerwang <rexercn@gmail.com>
2024-01-09 19:44:44 +0800

    build(components): add cssnano to minify css

--- packages/components/postcss.config.js
+++ packages/components/postcss.config.js
@@ -1,1 +1,3 @@
-module.exports = require('@min-kit/helper/config').postcss({ tailwindcss: true })
+const { plugins } = require('@min-kit/helper/config').postcss({ tailwindcss: true })
+
+module.exports = { plugins: plugins.concat([require('cssnano')]) }
