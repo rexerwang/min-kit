e43a7231c36a7150b89d8f92413f1430e18f374e
rexerwang <rexercn@gmail.com>
2024-01-10 21:37:54 +0800

    refactor(extends)!: remove `selectNodeContext` (useless)

--- packages/extends/jest.config.js
+++ packages/extends/jest.config.js
@@ -1,10 +1,1 @@
-module.exports = require('@min-kit/jest/config')({
-  collectCoverageFrom: [
-    '!src/config.ts',
-    '!src/current.ts',
-    '!src/document.ts',
-    '!src/storage.ts',
-    '!src/selector.ts',
-    '!src/systemInfo.ts',
-  ],
-})
+module.exports = require('@min-kit/jest/config')({})
